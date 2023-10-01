33536a2a35a584e5ac160e70bf20100d879f2cd3
Chimbori <hello@chimbori.com>
2023-10-01 00:22:15 -0700

    Add Dokka plugin to the Version Catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
   dependencies {
     classpath(libs.android.gradle.plugin)
     classpath(libs.kotlin.plugin)
-    classpath("org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.18")
+    classpath(libs.jetbrains.dokka.plugin)
     // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle files
   }
